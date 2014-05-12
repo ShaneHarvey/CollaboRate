205220a49a6ba65481be77a40e300f800b5f940f
Shane Harvey <shane6harvey@yahoo.com>
2014-05-12 01:22:24 -0400

    Added links to search results

--- src/search/SearchService.java
+++ src/search/SearchService.java
@@ -2,9 +2,13 @@ package search;
 
 import java.util.ArrayList;
 
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
 import account.Account;
 import material.Notes;
 import material.Question;
+import material.Subtopic;
 import material.Video;
 import discussion_board.Comment;
 import discussion_board.Post;
@@ -15,7 +19,13 @@ public class SearchService {
 		// Search all Posts
 		for(Post p: Post.search(query)){
 			SearchResult r = new SearchResult();
-			r.setType("Post");
+			Key pid = p.getKey();
+			Key stid = Post.getPost(pid).getSubtopic();
+			Key sid = Subtopic.getSubtopic(stid).getSubjectKey();
+			r.setType("<a href=\"/discuss?sid="+
+					KeyFactory.keyToString(sid)+"&stid="+
+					KeyFactory.keyToString(stid)+"&pid="+
+					KeyFactory.keyToString(pid)+"\">Post</a>");
 			r.setTitle(p.getBody());
 			r.setDate(p.getDate());
 			r.setAuthor(p.getAuthor().getDisplayNameOrEmail());
@@ -24,7 +34,12 @@ public class SearchService {
 		// Search all Comments
 		for(Comment c: Comment.search(query)){
 			SearchResult r = new SearchResult();
-			r.setType("Comment");
+			Key pid = c.getPostID();
+			Key stid = Post.getPost(pid).getSubtopic();
+			Key sid = Subtopic.getSubtopic(stid).getSubjectKey();
+			r.setType("<a href=\"/discuss?sid="+KeyFactory.keyToString(sid)+
+					"&stid="+KeyFactory.keyToString(stid)+
+					"&pid="+KeyFactory.keyToString(pid)+"\">Comment</a>");
 			r.setTitle(c.getBody());
 			r.setDate(c.getDate());
 			r.setAuthor(c.getAuthor().getDisplayNameOrEmail());
@@ -33,7 +48,7 @@ public class SearchService {
 		// Search all Questions
 		for(Question q: Question.search(query)){
 			SearchResult r = new SearchResult();
-			r.setType("Question");
+			r.setType("<a href=\"/question?qid="+KeyFactory.keyToString(q.getKey())+"\">Question</a>");
 			r.setTitle(q.getShortTitle());
 			r.setDate(q.getDate());
 			r.setAuthor(Account.getAccount(q.getAuthor()).getDisplayNameOrEmail());
@@ -42,7 +57,7 @@ public class SearchService {
 		// Search all Notes
 		for(Notes n: Notes.search(query)){
 			SearchResult r = new SearchResult();
-			r.setType("Note");
+			r.setType("<a href=\""+n.getURL()+"\">Note</a>");
 			r.setTitle(n.getTitle());
 			r.setDate(n.getDate());
 			r.setAuthor(Account.getAccount(n.getAuthor()).getDisplayNameOrEmail());
@@ -51,7 +66,7 @@ public class SearchService {
 		// Search all Videos
 		for(Video v: Video.search(query)){
 			SearchResult r = new SearchResult();
-			r.setType("Video");
+			r.setType("<a href=\"/video?vid="+KeyFactory.keyToString(v.getKey())+"\">Video</a>");
 			r.setTitle(v.getTitle());
 			r.setDate(v.getDate());
 			r.setAuthor(Account.getAccount(v.getAuthor()).getDisplayNameOrEmail());
